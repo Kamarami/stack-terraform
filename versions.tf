4bfd992beea9cacd9519c039e022c4da705d6a1e
Mohamed Kamara <mohamedxkamara@gmail.com>
2021-10-19 16:19:54 -0400

    versions

--- versions.tf
+++ versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.12"
 }
 
-provider "aws" {
+/*provider "aws" {
   profile = "default"
   region = "us-east-1"
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
-}
+}*/
