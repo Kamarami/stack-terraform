8549e13834b03fe4165050af9f51c09bee2634f3
Mohamed Kamara <mohamedxkamara@gmail.com>
2021-10-19 16:22:23 -0400

    Keys in main tf

--- STACK-EC2.tf
+++ STACK-EC2.tf
@@ -1,5 +1,7 @@
 terraform{
   backend "s3"{
+    AWS_ACCESS_KEY="AKIAYWH4KYIQMIGGM236"
+    AWS_SECRET_KEY="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"
     bucket= "stackbuckstatemohamed1"
     key = "terraform.tfstate"
     region="us-east-1"
