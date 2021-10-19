a8322a5c6284dbda46daf17c964b763c64cc970c
Mohamed Kamara <mohamedxkamara@gmail.com>
2021-10-19 19:49:55 -0400

    versions, deleted provider

--- versions.tf
+++ versions.tf
@@ -5,6 +5,6 @@ terraform {
 provider "aws" {
   profile = "default"
   region = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
+  /*access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY*/
 }
