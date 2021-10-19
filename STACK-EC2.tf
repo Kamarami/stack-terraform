757018e89f5493c1047181ad9c1c766a3de13fdd
Mohamed Kamara <mohamedxkamara@gmail.com>
2021-10-19 16:10:54 -0400

    versions

--- STACK-EC2.tf
+++ STACK-EC2.tf
@@ -1,5 +1,7 @@
 terraform{
   backend "s3"{
+    access_key = var.AWS_ACCESS_KEY
+    secret_key = var.AWS_SECRET_KEY
     bucket= "stackbuckstatemohamed1"
     key = "terraform.tfstate"
     region="us-east-1"
