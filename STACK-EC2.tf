247024348160d271c49def931b8502b5b17cf171
Mohamed Kamara <mohamedxkamara@gmail.com>
2021-10-19 21:58:12 -0400

    keys in main.tf

--- STACK-EC2.tf
+++ STACK-EC2.tf
@@ -5,5 +5,7 @@ terraform{
     region="us-east-1"
     dynamodb_table="statelock-tf"
     profile = "default"
+    access_key = "AKIAYWH4KYIQMIGGM236"
+    secret_key = "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"
   }
 }
